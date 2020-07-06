f776388ee1028c8dcb98c969f43cf52e4886ca38
Crystal Young <crystal.young@codeimmersives.com>
2020-07-06 13:12:16 -0400

    add

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+const capitalizeSentences = function (str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      result = result + str[i].toUpperCase();  
+  }
+}  
+return result 
+}
+
+
+
+function isValidPassword(str) {
+  str = str + ' ' === ' ' || str <= 12; {
+    return false;
+  }
+}
+
+
+function makeHalfSquares(arr) {
+  let result = arr
+    for (let i = 0; i < arr.length; i++) {
+      if (arr.length() % 2 === 0) {
+        return result
+          } else if  
+            (str.length() / 2)
+          return result;
+        }
+      }
+     
+  
+const countAs = function (nums) {
+  let result = nums;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums > 90) {
+      return result
+    }
+  }
+}
+
+
+function deleteMiddleLetters() {
+  
+}
 
 
 if (typeof capitalizeSentences === 'undefined') {
